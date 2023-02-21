c2741d4b9ba9c7cd83d2135ba596e7c701f43def
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-02-21 23:13:34 +0900

    BOJ : 02615 S1 오목

--- 완전탐색/BOJ_02615_오목.java
+++ 완전탐색/BOJ_02615_오목.java
@@ -1,4 +1,3 @@
-package Brute_force;
 /**
  * BOJ : 02615 S1 오목
  */
@@ -9,104 +8,80 @@ import java.util.StringTokenizer;
 
 public class BOJ_02615_오목 {
 
-    static int[][] board;
-    static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-    static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
-    static int startX, startY, endX, endY;
+    static int[][] map;
+    static int[] dx = {-1, 0, 1, 1};
+    static int[] dy = {1, 1, 1, 0};
+    static int resColor, resX, resY;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
 
-        board = new int[19][19];
-        for (int i = 0; i < 19; i++) {
+        map = new int[20][20];
+        for (int i = 1; i < 20; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 19; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 1; j < 20; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int resColor = 0;
-        boolean flag = false;
-        for (int i = 0; i < 19; i++) {
-            for (int j = 0; j < 19; j++) {
-                if (board[i][j] == 0) continue;
-                else if (board[i][j] == 1) {
-                    if (checkFive(i, j, 1)) {
-                        resColor = 1;
-                        startX = i + 1;
-                        startY = j + 1;
-                        flag = true;
-                    }
-                } else if (board[i][j] == 2) {
-                    if (checkFive(i, j, 2)) {
-                        resColor = 2;
-                        startX = i + 1;
-                        startY = j + 1;
-                        flag = true;
-                    }
-                }
-
-                if (flag) break;
-            }
-            if (flag) break;
-        }
-
-        int resX = startX;
-        int resY = startY;
-
-        if (startY < endY) {
-            resX = startX;
-            resY = startY;
-        } else if (startY > endY) {
-            resX = endX;
-            resY = endY;
-        } else {
-            if (startX < endX) {
-                resX = startX;
-                resY = startY;
-            } else if (startX > endX) {
-                resX = endX;
-                resY = endY;
-            }
-        }
-
-        System.out.println(resColor);
-        if (resColor != 0) {
+        if (solve()) {
+            System.out.println(resColor);
             System.out.println(resX + " " + resY);
+        } else {
+            System.out.println(resColor);
         }
     }
 
+    private static boolean solve() {
+        for (int i = 1; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                if (map[i][j] != 0) {
+                    if (checkFive(i, j, map[i][j])) {
+                        resColor = map[i][j];
+                        resX = i;
+                        resY = j;
+                        return true;
+                    }
+                }
+            }
+        }
+
+        resColor = 0;
+        return false;
+    }
+
     private static boolean checkFive(int x, int y, int color) {
-        int cnt = 0;
-        for (int d = 0; d < 8; d++) {
-            cnt = 0;
+        for (int d = 0; d < 4; d++) {
             int nx = x;
             int ny = y;
+            int cnt = 1;
 
             while (true) {
-                if (nx < 0 || nx >= 19 || ny < 0 || ny >= 19 || board[nx][ny] != color) break;
-
                 nx += dx[d];
                 ny += dy[d];
-            }
 
-            while (true) {
-                nx -= dx[d];
-                ny -= dy[d];
-
-                if (nx < 0 || nx >= 19 || ny < 0 || ny >= 19 || board[nx][ny] != color) break;
+                if (!isPossible(nx, ny) || map[nx][ny] != color) break;
 
                 cnt++;
             }
 
             if (cnt == 5) {
-                endX = nx+dx[d]+1;
-                endY = ny+dy[d]+1;
-                return true;
+                nx = x - dx[d];
+                ny = y - dy[d];
+
+                if (map[nx][ny] != color) return true;
+                else return false;
             }
         }
 
+        return false;
+    }
+
+    private static boolean isPossible(int nx, int ny) {
+        if (nx > 0 && nx <= 19 && ny > 0 && ny <= 19) {
+            return true;
+        }
         return false;
     }
 }
