9f37c8ac8d96d57ed481ec3990674ca196f9bfd6
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-07-19 17:40:21 +0900

    BOJ : 14940 S1 쉬운 최단거리

---
+++ 그래프탐색/BOJ_14940_쉬운최단거리.java
@@ -0,0 +1,91 @@
+/**
+ * BOJ : 14940 S1 쉬운 최단거리
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_14940_쉬운최단거리 {
+
+    static int N, M;
+    static int[][] map, dist;
+    static Queue<Point> Q;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        dist = new int[N][M];
+        Q = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                dist[i][j] = -1;
+                if (map[i][j] == 2) {
+                    dist[i][j] = 0;
+                    Q.add(new Point(i, j));
+                } else if (map[i][j] == 0) {
+                    dist[i][j] = 0;
+                }
+            }
+        }
+
+        BFS();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                sb.append(dist[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void BFS() {
+        while (!Q.isEmpty()) {
+            Point now = Q.poll();
+            int x = now.x;
+            int y = now.y;
+
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+
+                if (!isPossible(nx, ny)) continue;
+                if (map[nx][ny] != 1) continue;
+                if (dist[nx][ny] != -1) continue;
+
+                dist[nx][ny] = dist[x][y] + 1;
+                Q.add(new Point(nx, ny));
+            }
+        }
+    }
+
+    private static boolean isPossible(int nx, int ny) {
+        if (nx >= 0 && nx < N && ny >= 0 && ny < M) return true;
+        else return false;
+    }
+
+    static class Point {
+        int x;
+        int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
