c73c5514b840985b4515792e9ac42b0d3c20138e
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-08-04 20:02:48 +0900

    BOJ : 14888 S1 연산자 끼워넣기

--- 백트래킹/BOJ_14888_연산자끼워넣기.java
+++ 백트래킹/BOJ_14888_연산자끼워넣기.java
@@ -1,4 +1,3 @@
-package 백트래킹;
 /**
  * BOJ : 14888 S1 연산자 끼워넣기
  */
@@ -9,64 +8,76 @@ import java.util.StringTokenizer;
 
 public class BOJ_14888_연산자끼워넣기 {
 
-    static int N, resMax, resMin;
-    static int[] num, operator;
+    static int N, max, min, tmp;
+    static int[] num, op, selected;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
-        StringBuilder sb = new StringBuilder();
+        sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
-        resMax = Integer.MIN_VALUE;
-        resMin = Integer.MAX_VALUE;
 
         num = new int[N];
-        operator = new int[4];
-
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             num[i] = Integer.parseInt(st.nextToken());
         }
 
+        op = new int[4];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; i++) {
-            operator[i] = Integer.parseInt(st.nextToken());
+            op[i] = Integer.parseInt(st.nextToken());
         }
 
-        backtrack(1, num[0]);
+        max = Integer.MIN_VALUE;
+        min = Integer.MAX_VALUE;
+        selected = new int[N - 1];
 
-        sb.append(resMax).append("\n").append(resMin);
+        backtrack(0);
+
+        sb.append(max).append("\n").append(min);
         System.out.println(sb);
     }
 
-    private static void backtrack(int idx, int cal) {
-        if (idx == N) {
-            resMax = Math.max(resMax, cal);
-            resMin = Math.min(resMin, cal);
-        } else {
-            for (int i = 0; i < 4; i++) {
-                if (operator[i] > 0) {
-                    operator[i]--;
-
-                    switch (i) {
-                        case 0:
-                            backtrack(idx + 1, cal + num[idx]);
-                            break;
-                        case 1:
-                            backtrack(idx + 1, cal - num[idx]);
-                            break;
-                        case 2:
-                            backtrack(idx + 1, cal * num[idx]);
-                            break;
-                        case 3:
-                            backtrack(idx + 1, cal / num[idx]);
-                            break;
-                    }
-
-                    operator[i]++;
-                }
+    private static void backtrack(int depth) {
+        if (depth == N - 1) {
+            tmp = num[0];
+            for (int i = 0; i < N - 1; i++) {
+                calc(selected[i], num[i + 1]);
             }
+
+            max = Math.max(max, tmp);
+            min = Math.min(min, tmp);
+
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (op[i] == 0) continue;
+
+            op[i]--;
+            selected[depth] = i;
+            backtrack(depth + 1);
+            op[i]++;
+        }
+    }
+
+    private static void calc(int op, int n) {
+        switch (op) {
+            case 0:
+                tmp += n;
+                break;
+            case 1:
+                tmp -= n;
+                break;
+            case 2:
+                tmp *= n;
+                break;
+            case 3:
+                tmp /= n;
+                break;
         }
     }
 }
