9617d99daa633c1d706ac153ddd78188d04fa6bc
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-08-06 17:43:22 +0900

    BOJ : 10597 S1 순열장난

--- 백트래킹/BOJ_10597_순열장난.java
+++ 백트래킹/BOJ_10597_순열장난.java
@@ -7,63 +7,64 @@ import java.io.InputStreamReader;
 
 public class BOJ_10597_순열장난 {
 
-    static int N;
+    static int N, len;
     static int[] res;
     static String str;
-    static boolean flag;
     static boolean[] visited;
+    static boolean flag;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
 
         str = br.readLine();
 
-        int len = str.length();
+        len = str.length();
 
         if (len < 10) {
             N = len;
-            res = new int[N];
-            visited = new boolean[N + 1];
         } else {
             N = 9 + (len - 9) / 2;
-            res = new int[N];
-            visited = new boolean[N + 1];
         }
 
+        res = new int[N];
+        visited = new boolean[N + 1];
         flag = false;
+
         backtrack(0, 0);
+
+        System.out.println(sb);
     }
 
     private static void backtrack(int depth, int idx) {
-        if (depth >= N) {
-            for (int i = 0; i < res.length; i++) {
-                System.out.print(res[i] + " ");
+        if (flag) return;
+
+        if (depth == N) {
+            for (int i = 0; i < N; i++) {
+                sb.append(res[i]).append(" ");
             }
             flag = true;
             return;
         }
-
-        if (flag) return;
 
         int one = str.charAt(idx) - '0';
         if (one != 0 && !visited[one]) {
             visited[one] = true;
             res[depth] = one;
             backtrack(depth + 1, idx + 1);
-            if (flag) return;
             visited[one] = false;
         }
 
-        if (idx + 1 < str.length()) {
+        if (idx + 1 < len) {
             int two = Integer.parseInt(str.substring(idx, idx + 2));
-
             if (two <= N && !visited[two]) {
                 visited[two] = true;
                 res[depth] = two;
                 backtrack(depth + 1, idx + 2);
-                if (flag) return;
                 visited[two] = false;
             }
         }
+
     }
 }
