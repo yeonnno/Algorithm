153857431e1559bf5b387597e16c8d93dc0d912b
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-08-20 17:23:20 +0900

    BOJ : 16938 G5 캠프 준비

---
+++ 백트래킹/BOJ_16938_캠프준비.java
@@ -0,0 +1,52 @@
+/**
+ * BOJ : 16938 G5 캠프 준비
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_16938_캠프준비 {
+
+    static int N, L, R, X, res;
+    static int[] A;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+
+        A = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        res = 0;
+        Arrays.sort(A);
+
+        backtrack(0, 0, 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+        System.out.println(res);
+    }
+
+    private static void backtrack(int depth, int sum, int cnt, int max, int min) {
+        if (depth == N) {
+            if (cnt < 2) return;
+            if (sum < L || sum > R) return;
+
+            if (max - min >= X) res++;
+
+            return;
+        }
+
+        backtrack(depth + 1, sum + A[depth], cnt + 1, Math.max(max, A[depth]), Math.min(min, A[depth]));
+        backtrack(depth + 1, sum, cnt, max, min);
+    }
+}
