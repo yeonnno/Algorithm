1df6badcccaa141de891f22e36554075e690badf
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-08-23 17:33:56 +0900

    BOJ : 19942 G5 다이어트

--- 백트래킹/BOJ_19942_다이어트.java
+++ 백트래킹/BOJ_19942_다이어트.java
@@ -9,8 +9,8 @@ import java.util.StringTokenizer;
 public class BOJ_19942_다이어트 {
 
     static int N, res;
-    static int[] nutrient, selected;
-    static int[][] ingredient;
+    static int[] nutrients, selected;
+    static int[][] ingredients;
     static boolean[] visited;
     static boolean flag;
 
@@ -21,17 +21,17 @@ public class BOJ_19942_다이어트 {
 
         N = Integer.parseInt(br.readLine());
 
+        nutrients = new int[4];
         st = new StringTokenizer(br.readLine());
-        nutrient = new int[4];
         for (int i = 0; i < 4; i++) {
-            nutrient[i] = Integer.parseInt(st.nextToken());
+            nutrients[i] = Integer.parseInt(st.nextToken());
         }
 
-        ingredient = new int[N][5];
+        ingredients = new int[N][5];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 5; j++) {
-                ingredient[i][j] = Integer.parseInt(st.nextToken());
+                ingredients[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
@@ -44,39 +44,37 @@ public class BOJ_19942_다이어트 {
         if (flag) {
             sb.append(res).append("\n");
             for (int i = 0; i < N; i++) {
-                if (selected[i] != 0) {
+                if (selected[i] != 0)
                     sb.append(selected[i]).append(" ");
-                }
             }
             sb.append("\n");
-            System.out.println(sb);
         } else {
-            System.out.println(-1);
+            sb.append(-1).append("\n");
         }
+
+        System.out.println(sb);
     }
 
     private static void backtrack(int depth, int p, int f, int s, int v, int c) {
-        if (p >= nutrient[0] && f >= nutrient[1] && s >= nutrient[2] && v >= nutrient[3]) {
+        if (p >= nutrients[0] && f >= nutrients[1] && s >= nutrients[2] && v >= nutrients[3]) {
             flag = true;
 
             if (res > c) {
                 res = c;
+
                 int idx = 0;
                 selected = new int[N];
                 for (int i = 0; i < N; i++) {
-                    if (visited[i]) {
-                        selected[idx++] = i+1;
-                    }
+                    if (visited[i]) selected[idx++] = i+1;
                 }
             }
-
             return;
         }
 
         if (depth == N) return;
 
         visited[depth] = true;
-        backtrack(depth + 1, p + ingredient[depth][0], f + ingredient[depth][1], s + ingredient[depth][2], v + ingredient[depth][3], c + ingredient[depth][4]);
+        backtrack(depth + 1, p + ingredients[depth][0], f + ingredients[depth][1], s + ingredients[depth][2], v + ingredients[depth][3], c + ingredients[depth][4]);
         visited[depth] = false;
 
         backtrack(depth + 1, p, f, s, v, c);
