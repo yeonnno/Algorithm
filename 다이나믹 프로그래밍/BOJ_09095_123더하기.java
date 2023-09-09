03215440e266c8261a2323f339470dd22fac9ba3
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-09-09 18:35:09 +0900

    BOJ : 9095 S3 1,2,3 더하기

---
+++ 다이나믹 프로그래밍/BOJ_09095_123더하기.java
@@ -0,0 +1,34 @@
+/**
+ * BOJ : 9095 S3 1,2,3 더하기
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_09095_123더하기 {
+
+    static int T, N;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        dp = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i < 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            N = Integer.parseInt(br.readLine());
+
+            sb.append(dp[N]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
