51f7185d5da5d8f091ff13a8551668c8534cc54d
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-09-09 18:18:51 +0900

    BOJ : 1463 S3 1로 만들기

---
+++ 다이나믹 프로그래밍/BOJ_01463_1로만들기.java
@@ -0,0 +1,26 @@
+/**
+ * BOJ : 1463 S3 1로 만들기
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_01463_1로만들기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+
+        System.out.println(dp[N]);
+    }
+}
