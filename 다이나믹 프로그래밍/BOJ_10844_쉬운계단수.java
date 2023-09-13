5f0af74f0608c7518855035730bbe3e6e9840682
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-09-13 19:53:00 +0900

    BOJ : 10844 S1 쉬운 계단 수

---
+++ 다이나믹 프로그래밍/BOJ_10844_쉬운계단수.java
@@ -0,0 +1,43 @@
+/**
+ * BOJ : 10844 S1 쉬운 계단 수
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10844_쉬운계단수 {
+
+    static int N, mod = 1000000000;
+    static long res;
+    static long[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        dp = new long[N + 1][10];
+        for (int i = 1; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j == 0) {
+                    dp[i][0] = dp[i - 1][1] % mod;
+                } else if (j == 9) {
+                    dp[i][9] = dp[i - 1][8] % mod;
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
+                }
+            }
+        }
+
+        res = 0;
+        for (int i = 0; i < 10; i++) {
+            res += dp[N][i];
+        }
+
+        System.out.println(res % mod);
+    }
+}
