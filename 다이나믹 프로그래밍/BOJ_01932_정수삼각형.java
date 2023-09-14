b667b3d4d4be0c8cff54bad915a9344e8b9ca53c
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-09-14 17:22:27 +0900

    BOJ : 1932 S1 정수 삼각형

---
+++ 다이나믹 프로그래밍/BOJ_01932_정수삼각형.java
@@ -0,0 +1,42 @@
+/**
+ * BOJ : 1932 S1 정수 삼각형
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_01932_정수삼각형 {
+
+    static int N, res;
+    static int[][] map, dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= i; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + map[i][j];
+            }
+        }
+
+        res = 0;
+        for (int i = 1; i <= N; i++) {
+            if (res < dp[N][i]) res = dp[N][i];
+        }
+
+        System.out.println(res);
+    }
+}
