b0de74255c723143136f94864c8f92ddd38621bd
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-09-14 16:25:44 +0900

    BOJ : 1149 S1 RGB거리

--- 다이나믹 프로그래밍/BOJ_01149_RGB거리.java
+++ 다이나믹 프로그래밍/BOJ_01149_RGB거리.java
@@ -17,7 +17,7 @@ public class BOJ_01149_RGB거리 {
 
         N = Integer.parseInt(br.readLine());
 
-        map = new int[N][N];
+        map = new int[N][3];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
@@ -25,24 +25,15 @@ public class BOJ_01149_RGB거리 {
             }
         }
 
-        dp = new int[N][N];
-        for (int i = 0; i < 3; i++) {
-            dp[0][i] = map[0][i];
-        }
+        dp = new int[N][3];
+        dp[0][0] = map[0][0]; dp[0][1] = map[0][1]; dp[0][2] = map[0][2];
 
         for (int i = 1; i < N; i++) {
-            dp[i][0] = map[i][0] + Math.min(dp[i-1][1], dp[i-1][2]);
-            dp[i][1] = map[i][1] + Math.min(dp[i-1][0], dp[i-1][2]);
-            dp[i][2] = map[i][2] + Math.min(dp[i-1][0], dp[i-1][1]);
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + map[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + map[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + map[i][2];
         }
 
-        int res = Integer.MAX_VALUE;
-        for (int i = 0; i < 3; i++) {
-            if (res > dp[N - 1][i]) {
-                res = dp[N - 1][i];
-            }
-        }
-
-        System.out.println(res);
+        System.out.println(Math.min(dp[N - 1][0], Math.min(dp[N - 1][1], dp[N - 1][2])));
     }
 }
