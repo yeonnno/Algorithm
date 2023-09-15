744c0ec08341fe4ae304ea19ce89d31057d00630
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-09-15 16:28:10 +0900

    BOJ : 11052 S1 카드 구매하기

---
+++ 다이나믹 프로그래밍/BOJ_11052_카드구매하기.java
@@ -0,0 +1,35 @@
+/**
+ * BOJ : 11052 S1 카드 구매하기
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11052_카드구매하기 {
+
+    static int N;
+    static int[] map, dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            map[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + map[j]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
