7e872e7980cb636b99773da2141ecccb5b5d5775
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-09-16 19:33:51 +0900

    BOJ : 1309 S1 동물원

---
+++ 다이나믹 프로그래밍/BOJ_01309_동물원.java
@@ -0,0 +1,29 @@
+/**
+ * BOJ : 1309 S1 동물원
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_01309_동물원 {
+
+    static int N, MOD = 9901;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        dp = new int[N + 1][3];
+        dp[1][0] = 1; dp[1][1] = 1; dp[1][2] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % MOD;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+        }
+
+        System.out.println((dp[N][0] + dp[N][1] + dp[N][2]) % MOD);
+    }
+}
