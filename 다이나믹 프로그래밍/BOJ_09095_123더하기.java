0a908607f34acfde2459c99ebbc3024c1ad24f70
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-09-17 17:34:13 +0900

    BOJ : 9095 S3 1,2,3 더하기

--- 다이나믹 프로그래밍/BOJ_09095_123더하기.java
+++ 다이나믹 프로그래밍/BOJ_09095_123더하기.java
@@ -15,9 +15,7 @@ public class BOJ_09095_123더하기 {
         StringBuilder sb = new StringBuilder();
 
         dp = new int[11];
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
+        dp[1] = 1; dp[2] = 2; dp[3] = 4;
         for (int i = 4; i < 11; i++) {
             dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
