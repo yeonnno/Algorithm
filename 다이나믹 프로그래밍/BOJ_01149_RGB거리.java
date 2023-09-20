b5a99d80396d06ace1d141c262be11452420cabf
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-09-20 16:53:45 +0900

    BOJ : 1149 S1 RGB거리

--- 다이나믹 프로그래밍/BOJ_01149_RGB거리.java
+++ 다이나믹 프로그래밍/BOJ_01149_RGB거리.java
@@ -26,7 +26,9 @@ public class BOJ_01149_RGB거리 {
         }
 
         dp = new int[N][3];
-        dp[0][0] = map[0][0]; dp[0][1] = map[0][1]; dp[0][2] = map[0][2];
+        dp[0][0] = map[0][0];
+        dp[0][1] = map[0][1];
+        dp[0][2] = map[0][2];
 
         for (int i = 1; i < N; i++) {
             dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + map[i][0];
