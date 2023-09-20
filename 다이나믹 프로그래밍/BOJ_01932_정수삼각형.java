955fb3750a4a6a522bebd61e6bc4998e08f8715c
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-09-20 17:47:13 +0900

    BOJ : 1932 S1 정수 삼각형

--- 다이나믹 프로그래밍/BOJ_01932_정수삼각형.java
+++ 다이나믹 프로그래밍/BOJ_01932_정수삼각형.java
@@ -28,13 +28,13 @@ public class BOJ_01932_정수삼각형 {
         dp = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= i; j++) {
-                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + map[i][j];
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1]) + map[i][j];
             }
         }
 
         res = 0;
         for (int i = 1; i <= N; i++) {
-            if (res < dp[N][i]) res = dp[N][i];
+            res = Math.max(res, dp[N][i]);
         }
 
         System.out.println(res);
