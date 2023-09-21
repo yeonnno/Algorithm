c94550a9660865ef5d5e70ca01d854a523ef7cf9
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-09-21 16:52:31 +0900

    BOJ : 1309 S1 동물원

--- 다이나믹 프로그래밍/BOJ_01309_동물원.java
+++ 다이나믹 프로그래밍/BOJ_01309_동물원.java
@@ -16,7 +16,9 @@ public class BOJ_01309_동물원 {
         N = Integer.parseInt(br.readLine());
 
         dp = new int[N + 1][3];
-        dp[1][0] = 1; dp[1][1] = 1; dp[1][2] = 1;
+        dp[1][0] = 1;
+        dp[1][1] = 1;
+        dp[1][2] = 1;
 
         for (int i = 2; i <= N; i++) {
             dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % MOD;
