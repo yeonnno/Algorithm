e5fa0874b8a2d224cb0f2bc2d9ab322e47fffe45
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-10-06 17:29:43 +0900

    BOJ : 11403 S1 경로 찾기

--- 최단경로/BOJ_11403_경로찾기.java
+++ 최단경로/BOJ_11403_경로찾기.java
@@ -18,18 +18,18 @@ public class BOJ_11403_경로찾기 {
 
         N = Integer.parseInt(br.readLine());
 
-        adj = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
+        adj = new int[N][N];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
+            for (int j = 0; j < N; j++) {
                 adj[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         floyd();
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 sb.append(adj[i][j]).append(" ");
             }
             sb.append("\n");
@@ -39,12 +39,10 @@ public class BOJ_11403_경로찾기 {
     }
 
     private static void floyd() {
-        for (int k = 1; k <= N; k++) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    if (adj[i][k] == 1 && adj[k][j] == 1) {
-                        adj[i][j] = 1;
-                    }
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (adj[i][k] == 1 && adj[k][j] == 1) adj[i][j] = 1;
                 }
             }
         }
