9b93657bec70251251f8d9c0993163b6ca09e67a
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-10-17 15:41:14 +0900

    BOJ : 11404 G4 플로이드

---
+++ 최단경로/BOJ_11404_플로이드.java
@@ -0,0 +1,63 @@
+/**
+ * BOJ : 11404 G4 플로이드
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11404_플로이드 {
+
+    static int N, M, INF = 999999999;
+    static int[][] adj;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        adj = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i != j) adj[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            adj[s][e] = Math.min(adj[s][e], w);
+        }
+
+        floyd();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (adj[i][j] == INF) sb.append(0);
+                else sb.append(adj[i][j]);
+                sb.append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void floyd() {
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (i == j || j == k || k == i) continue;
+
+                    adj[i][j] = Math.min(adj[i][j], adj[i][k] + adj[k][j]);
+                }
+            }
+        }
+    }
+}
