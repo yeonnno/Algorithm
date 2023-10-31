73e311352991b13c4ef039ded3e99bb2d286af34
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-10-31 16:29:16 +0900

    BOJ : 1719 G3 택배

---
+++ 최단경로/BOJ_01719_택배.java
@@ -0,0 +1,73 @@
+/**
+ * BOJ : 1719 G3 택배
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_01719_택배 {
+
+    static int N, M, INF = 999999999;
+    static int[][] adj, res;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        adj = new int[N + 1][N + 1];
+        res = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i != j){
+                    adj[i][j] = INF;
+                    res[i][j] = j;
+                }
+
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            adj[s][e] = w;
+            adj[e][s] = w;
+        }
+
+        floyd();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) sb.append("-");
+                else sb.append(res[i][j]);
+                sb.append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void floyd() {
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (i == j || j == k || k == i) continue;
+
+                    if (adj[i][j] > adj[i][k] + adj[k][j]) {
+                        adj[i][j] = adj[i][k] + adj[k][j];
+                        res[i][j] = res[i][k];
+                    }
+                }
+            }
+        }
+    }
+}
