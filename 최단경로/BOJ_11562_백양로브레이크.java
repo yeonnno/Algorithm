2a7f29b0fd86c234c904d9e81cdeda4816cae8fd
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-11-05 16:14:09 +0900

    BOJ : 11562 G3 백양로 브레이크

---
+++ 최단경로/BOJ_11562_백양로브레이크.java
@@ -0,0 +1,72 @@
+/**
+ * BOJ : 11562 G3 백양로 브레이크
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11562_백양로브레이크 {
+
+    static int N, M, K, INF = 999999999;
+    static int[][] adj;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        adj = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i != j) adj[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            adj[u][v] = 0;
+
+            if (b == 0) {
+                adj[v][u] = 1;
+            } else {
+                adj[v][u] = 0;
+            }
+        }
+
+        floyd();
+
+        K = Integer.parseInt(br.readLine());
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            sb.append(adj[s][e]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void floyd() {
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (i == j || j == k || k == i) continue;
+
+                    if (adj[i][j] > adj[i][k] + adj[k][j]) {
+                        adj[i][j] = adj[i][k] + adj[k][j];
+                    }
+                }
+            }
+        }
+    }
+}
