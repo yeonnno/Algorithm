deb3855620a034a610fabd176619ca6ce41b0725
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-11-21 17:37:11 +0900

    BOJ : 1749 G4 점수따먹기

---
+++ 누적합/BOJ_01749_점수따먹기.java
@@ -0,0 +1,52 @@
+/**
+ * BOJ : 1749 G4 점수따먹기
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_01749_점수따먹기 {
+
+    static int N, M;
+    static int[][] map, preSum;
+    static long max, res;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        preSum = new int[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                preSum[i][j] = preSum[i - 1][j] + preSum[i][j - 1] - preSum[i - 1][j - 1] + map[i][j];
+            }
+        }
+
+        res = Integer.MIN_VALUE;
+        for (int x1 = 1; x1 <= N; x1++) {
+            for (int y1 = 1; y1 <= M; y1++) {
+                for (int x2 = x1; x2 <= N; x2++) {
+                    for (int y2 = y1; y2 <= M; y2++) {
+                        max = preSum[x2][y2] - preSum[x1 - 1][y2] - preSum[x2][y1 - 1] + preSum[x1 - 1][y1 - 1];
+                        res = Math.max(res, max);
+                    }
+                }
+            }
+        }
+
+        System.out.println(res);
+    }
+}
