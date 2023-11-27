13fec5c3d03c2ae5a5440c6cdd79f585ccd8899e
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-11-27 16:22:27 +0900

    BOJ : 2512 S2 예산

---
+++ 이분탐색/BOJ_02512_예산.java
@@ -0,0 +1,46 @@
+/**
+ * BOJ : 2512 S2 예산
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_02512_예산 {
+
+    static int N, M;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        N = Integer.parseInt(br.readLine());
+
+        int left = 0, right = 0;
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            right = Math.max(right, arr[i]);
+        }
+
+        M = Integer.parseInt(br.readLine());
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            long budget = 0;
+
+            for (int a : arr) {
+                if (a > mid) budget += mid;
+                else budget += a;
+            }
+
+            if (budget <= M) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        System.out.println(right);
+    }
+}
