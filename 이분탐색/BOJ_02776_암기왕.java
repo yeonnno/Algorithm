cabbb7ec224769cf13451e3173d9e63b0d9c080c
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-12-11 16:36:06 +0900

    BOJ : 2776 S4 암기왕

--- 이분탐색/BOJ_02776_암기왕.java
+++ 이분탐색/BOJ_02776_암기왕.java
@@ -10,7 +10,7 @@ import java.util.StringTokenizer;
 public class BOJ_02776_암기왕 {
 
     static int N, M;
-    static int[] num;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,38 +21,41 @@ public class BOJ_02776_암기왕 {
         for (int t = 0; t < T; t++) {
             N = Integer.parseInt(br.readLine());
 
-            num = new int[N + 1];
+            arr = new int[N + 1];
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < N; i++) {
-                num[i] = Integer.parseInt(st.nextToken());
+                arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            Arrays.sort(num);
+            Arrays.sort(arr);
 
             M = Integer.parseInt(br.readLine());
+
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < M; i++) {
-                int question = Integer.parseInt(st.nextToken());
-                int left = 0;
-                int right = N;
-                boolean flag = false;
+                int num = Integer.parseInt(st.nextToken());
+                int left = 0, right = N;
+                boolean check = false;
 
                 while (left <= right) {
                     int mid = (left + right) / 2;
 
-                    if (num[mid] < question) left = mid + 1;
-                    else if (num[mid] > question) right = mid - 1;
-                    else {
-                        flag = true;
+                    if (arr[mid] > num) {
+                        right = mid - 1;
+                    } else if (arr[mid] < num) {
+                        left = mid + 1;
+                    } else {
+                        check = true;
                         break;
                     }
                 }
 
-                if (flag) sb.append(1);
+                if (check) sb.append(1);
                 else sb.append(0);
                 sb.append("\n");
             }
         }
+
         System.out.print(sb);
     }
 }
