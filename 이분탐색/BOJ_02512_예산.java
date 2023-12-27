7952b522968ca67e56f0ccf06000fa21433b79a8
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-12-27 17:41:08 +0900

    BOJ : 2512 S2 예산

--- 이분탐색/BOJ_02512_예산.java
+++ 이분탐색/BOJ_02512_예산.java
@@ -17,10 +17,9 @@ public class BOJ_02512_예산 {
 
         N = Integer.parseInt(br.readLine());
 
-        int left = 0, right = 0;
-
         arr = new int[N];
         st = new StringTokenizer(br.readLine());
+        int left = 0, right = 0;
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             right = Math.max(right, arr[i]);
@@ -29,7 +28,7 @@ public class BOJ_02512_예산 {
         M = Integer.parseInt(br.readLine());
 
         while (left <= right) {
-            int mid = (left + right) / 2;
+            int mid = (left + right) / 2; // 상한액
             long budget = 0;
 
             for (int a : arr) {
