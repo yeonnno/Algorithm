7ac8243e816dfbfc1304e3e44d3ab60d70a57efd
yeonnno <109949649+yeonnno@users.noreply.github.com>
2023-12-28 18:39:36 +0900

    BOJ : 16401 S2 과자 나눠주기

--- 이분탐색/BOJ_16401_과자나눠주기.java
+++ 이분탐색/BOJ_16401_과자나눠주기.java
@@ -9,7 +9,7 @@ import java.util.StringTokenizer;
 public class BOJ_16401_과자나눠주기 {
 
     static int M, N, res;
-    static int[] snack;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,22 +19,21 @@ public class BOJ_16401_과자나눠주기 {
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
 
-        int left = 1, right = 0;
-
-        snack = new int[N];
+        arr = new int[N];
         st = new StringTokenizer(br.readLine());
+        int left = 1, right = 0;
         for (int i = 0; i < N; i++) {
-            snack[i] = Integer.parseInt(st.nextToken());
-            right = Math.max(right, snack[i]);
+            arr[i] = Integer.parseInt(st.nextToken());
+            right = Math.max(right, arr[i]);
         }
 
         res = 0;
         while (left <= right) {
             int mid = (left + right) / 2;
-            int cnt = 0;
+            long cnt = 0;
 
-            for (int s : snack) {
-                cnt += s / mid;
+            for (int a : arr) {
+                cnt += a / mid;
             }
 
             if (cnt >= M) {
