f95ac67fa631065c625e9ff4ce2bf0066389a90c
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-01-22 16:32:55 +0900

    BOJ : 14938 G4 서강그라운드

--- 최단경로/BOJ_14938_서강그라운드.java
+++ 최단경로/BOJ_14938_서강그라운드.java
@@ -4,13 +4,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BOJ_14938_서강그라운드 {
 
     static int N, M, R, res, INF = 999999999;
-    static int[] item;
-    static int[][] adj;
+    static int[] item, dist;
+    static ArrayList<Node>[] adj;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,51 +31,74 @@ public class BOJ_14938_서강그라운드 {
             item[i] = Integer.parseInt(st.nextToken());
         }
 
-        adj = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (i != j) adj[i][j] = INF;
-            }
+        adj = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) {
+            adj[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < R; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
 
-            adj[s][e] = adj[e][s] = w;
+            adj[s].add(new Node(e, cost));
+            adj[e].add(new Node(s, cost));
         }
-
-        floyd();
 
         res = 0;
         for (int i = 1; i <= N; i++) {
-            int cnt = item[i];
+            dist = new int[N + 1];
+            Arrays.fill(dist, INF);
+            dist[i] = 0;
 
+            visited = new boolean[N + 1];
+
+            dijkstra(i);
+
+            int max = 0;
             for (int j = 1; j <= N; j++) {
-                if (i == j) continue;
-
-                if (adj[i][j] <= M) cnt += item[j];
+                if (dist[j] <= M) max += item[j];
             }
 
-            res = Math.max(res, cnt);
+            res = Math.max(res, max);
         }
 
         System.out.println(res);
     }
 
-    private static void floyd() {
-        for (int k = 1; k <= N; k++) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    if (i == j || j == k || k == i) continue;
+    private static void dijkstra(int start) {
+        PriorityQueue<Node> PQ = new PriorityQueue<>();
+        PQ.add(new Node(start, 0));
 
-                    if (adj[i][j] > adj[i][k] + adj[k][j]) {
-                        adj[i][j] = adj[i][k] + adj[k][j];
-                    }
+        while (!PQ.isEmpty()) {
+            Node now = PQ.poll();
+
+            if (visited[now.e]) continue;
+
+            visited[now.e] = true;
+
+            for (Node next : adj[now.e]) {
+                if (dist[next.e] > dist[now.e] + next.cost) {
+                    dist[next.e] = dist[now.e] + next.cost;
+                    PQ.add(new Node(next.e, dist[next.e]));
                 }
             }
+        }
+    }
+
+    private static class Node implements Comparable<Node> {
+        int e;
+        int cost;
+
+        Node(int e, int cost) {
+            this.e = e;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
         }
     }
 }
