29a03b6ae0da2dad6fe2983f5b5fee2487360ae1
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-01-25 17:24:33 +0900

    BOJ : 1956 G4 운동

--- 최단경로/BOJ_01956_운동.java
+++ 최단경로/BOJ_01956_운동.java
@@ -10,7 +10,6 @@ public class BOJ_01956_운동 {
 
     static int V, E, res, INF = 999999999;
     static int[][] adj;
-    static boolean check;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,25 +30,25 @@ public class BOJ_01956_운동 {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
 
-            adj[s][e] = w;
+            adj[s][e] = cost;
         }
 
         floyd();
 
-        check = false;
-        res = Integer.MAX_VALUE;
+        res = INF;
         for (int i = 1; i <= V; i++) {
-            for (int j = i + 1; j <= V; j++) {
+            for (int j = 1; j <= V; j++) {
+                if (i == j) continue;
+
                 if (adj[i][j] != INF && adj[j][i] != INF) {
-                    check = true;
                     res = Math.min(res, adj[i][j] + adj[j][i]);
                 }
             }
         }
 
-        if (check) System.out.println(res);
+        if (res != INF) System.out.println(res);
         else System.out.println(-1);
     }
 
