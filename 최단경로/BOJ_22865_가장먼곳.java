fc2e607f23ddc185dd3fcc3a19d8657f846e66a8
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-01-28 17:21:53 +0900

    BOJ : 22865 G4 가장 먼 곳

--- 최단경로/BOJ_22865_가장먼곳.java
+++ 최단경로/BOJ_22865_가장먼곳.java
@@ -11,7 +11,8 @@ import java.util.StringTokenizer;
 
 public class BOJ_22865_가장먼곳 {
 
-    static int N, M, res, INF = 999999999;
+    static int N, M, res;
+    static final int INF = 999999999;
     static int[] friends, dist, minDist;
     static ArrayList<Node>[] adj;
     static boolean[] visited;
@@ -28,37 +29,38 @@ public class BOJ_22865_가장먼곳 {
             friends[i] = Integer.parseInt(st.nextToken());
         }
 
-        M = Integer.parseInt(br.readLine());
-
         adj = new ArrayList[N + 1];
         for (int i = 0; i <= N; i++) {
             adj[i] = new ArrayList<>();
         }
 
+        M = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
 
-            adj[s].add(new Node(e, w));
-            adj[e].add(new Node(s, w));
+            adj[s].add(new Node(e, cost));
+            adj[e].add(new Node(s, cost));
         }
 
         dist = new int[N + 1];
         minDist = new int[N + 1];
         Arrays.fill(minDist, INF);
 
-        for (int i = 1; i <= N; i++) {
-            if (i != friends[0] && i != friends[1] && i != friends[2]) continue;
+        for (int friend : friends) {
+            Arrays.fill(dist, INF);
+            dist[friend] = 0;
 
             visited = new boolean[N + 1];
 
-            dijkstra(i);
+            dijkstra(friend);
         }
 
         res = 0;
-        int max = -1;
+        int max = -INF;
         for (int i = 1; i <= N; i++) {
             if (minDist[i] == INF) continue;
 
@@ -74,20 +76,17 @@ public class BOJ_22865_가장먼곳 {
     private static void dijkstra(int start) {
         PriorityQueue<Node> PQ = new PriorityQueue<>();
         PQ.add(new Node(start, 0));
-        Arrays.fill(dist, INF);
-        dist[start] = 0;
 
         while (!PQ.isEmpty()) {
             Node now = PQ.poll();
 
             if (visited[now.e]) continue;
+
             visited[now.e] = true;
 
             for (Node next : adj[now.e]) {
-                if (visited[next.e]) continue;
-
-                if (dist[next.e] > dist[now.e] + next.w) {
-                    dist[next.e] = dist[now.e] + next.w;
+                if (dist[next.e] > dist[now.e] + next.cost) {
+                    dist[next.e] = dist[now.e] + next.cost;
                     PQ.add(new Node(next.e, dist[next.e]));
                 }
             }
@@ -102,16 +101,16 @@ public class BOJ_22865_가장먼곳 {
 
     private static class Node implements Comparable<Node> {
         int e;
-        int w;
+        int cost;
 
-        Node(int e, int w) {
+        Node(int e, int cost) {
             this.e = e;
-            this.w = w;
+            this.cost = cost;
         }
 
         @Override
         public int compareTo(Node o) {
-            return this.w - o.w;
+            return this.cost - o.cost;
         }
     }
 }
