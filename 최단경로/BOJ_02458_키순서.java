bf54a1081d4c25c4f2a0710a40db7eed371c7dd3
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-02-06 16:34:16 +0900

    BOJ : 2458 G4 키 순서

--- 최단경로/BOJ_02458_키순서.java
+++ 최단경로/BOJ_02458_키순서.java
@@ -8,9 +8,10 @@ import java.util.StringTokenizer;
 
 public class BOJ_02458_키순서 {
 
-    static int N, M, res, INF = 999999999;
-    static int[][] adj;
-    static int[] check;
+    static int N, M, res;
+    static final int INF = 999999999;
+    static boolean[][] adj;
+    static int[] count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,34 +21,28 @@ public class BOJ_02458_키순서 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        adj = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (i != j) adj[i][j] = INF;
-            }
-        }
-
+        adj = new boolean[N + 1][N + 1];
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
 
-            adj[s][e] = 1;
+            adj[s][e] = true;
         }
 
         floyd();
 
-        check = new int[N + 1];
+        count = new int[N + 1];
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                if (adj[j][i] == 1) check[i]++;
-                if (adj[i][j] == 1) check[i]++;
+                if (adj[i][j]) count[i]++;
+                if (adj[j][i]) count[i]++;
             }
         }
 
         res = 0;
         for (int i = 1; i <= N; i++) {
-            if (check[i] == N - 1) res++;
+            if (count[i] == N - 1) res++;
         }
 
         System.out.println(res);
@@ -59,10 +54,7 @@ public class BOJ_02458_키순서 {
                 for (int j = 1; j <= N; j++) {
                     if (i == j || j == k || k == i) continue;
 
-                    if (adj[i][k] == 1 && adj[k][j] == 1) adj[i][j] = 1;
-//                    if (adj[i][j] > adj[i][k] + adj[k][j]) {
-//                        adj[i][j] = adj[i][k] + adj[k][j];
-//                    }
+                    if (adj[i][k] && adj[k][j]) adj[i][j] = true;
                 }
             }
         }
