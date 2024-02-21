badf611680f83ee019726070dec8709f20fa62ca
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-02-21 19:13:38 +0900

    PGM : LV2 멀리 뛰기

---
+++ PGM/LV2/PGM_LV2_멀리뛰기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(int n) {
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        
+        if (n >= 2) {
+            dp[2] = 2;
+            
+            for (int i = 3; i <= n; i++) {
+                dp[i] = (dp[i - 2] + dp[i - 1]) % 1234567;
+            }
+        }
+        
+        return dp[n] % 1234567;
+    }
+}
