7e771657d8ddf8158c298a248e320266511dc13e
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-02-26 15:30:37 +0900

    PGM : LV2 의상

---
+++ PGM/LV2/PGM_LV2_의상.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        
+        for (String key : map.keySet()) {
+            answer *= (map.get(key) + 1);
+        }
+        
+        answer--;
+        
+        return answer;
+    }
+}
