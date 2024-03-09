3a09d5a275b6c5a4eb55675acfe0d7d5ad3b27cf
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-03-09 14:42:08 +0900

    BOJ : 1647 G4 도시 분할 계획

---
+++ 최소 스패닝 트리/BOJ_01647_도시분할계획.java
@@ -0,0 +1,99 @@
+/**
+ * BOJ : 1647 G4 도시 분할 계획
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BOJ_01647_도시분할계획 {
+
+    static int N, M, res, max;
+    static int[] parent;
+    static PriorityQueue<Node> PQ;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        PQ = new PriorityQueue<>();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            PQ.offer(new Node(s, e, cost));
+        }
+
+        res = 0;
+        max = -1;
+
+        kruskal();
+
+        System.out.println(res - max);
+    }
+
+    private static void kruskal() {
+        while (!PQ.isEmpty()) {
+            Node node = PQ.poll();
+
+            int s = find(node.s);
+            int e = find(node.e);
+
+            if (isSameParent(s, e)) continue;
+
+            max = Math.max(max, node.cost);
+            res += node.cost;
+
+            union(node.s, node.e);
+        }
+    }
+
+    private static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x != y) parent[y] = x;
+    }
+
+    private static boolean isSameParent(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x == y) return true;
+        else return false;
+    }
+
+    private static int find(int x) {
+        if (parent[x] == x) return x;
+        else return parent[x] = find(parent[x]);
+    }
+
+    public static class Node implements Comparable<Node> {
+        int s;
+        int e;
+        int cost;
+
+        Node(int s, int e, int cost) {
+            this.s = s;
+            this.e = e;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
+}
