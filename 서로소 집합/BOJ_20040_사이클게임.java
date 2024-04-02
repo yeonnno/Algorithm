0d8dcbcd70f4ff7dd14b764e8fdf848893f54950
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-04-02 15:16:17 +0900

    BOJ : 20040 G4 사이클 게임

---
+++ 서로소 집합/BOJ_20040_사이클게임.java
@@ -0,0 +1,55 @@
+/**
+ * BOJ : 20040 G4 사이클 게임
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_20040_사이클게임 {
+
+    static int N, M, res;
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N];
+        for (int i = 0; i < N; i++) {
+            parent[i] = i;
+        }
+
+        res = 0;
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (find(x) == find(y)) {
+                res = i;
+                break;
+            } else {
+                union(x, y);
+            }
+        }
+
+        System.out.println(res);
+    }
+
+    private static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x != y) parent[y] = x;
+    }
+
+    private static int find(int x) {
+        if (parent[x] == x) return x;
+        else return parent[x] = find(parent[x]);
+    }
+}
