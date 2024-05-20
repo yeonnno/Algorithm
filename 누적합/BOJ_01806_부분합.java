7994ac6d53e0da2673d8acbcb25cb324516669d1
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-05-20 15:09:49 +0900

    BOJ : 1806 G4 부분합

---
+++ 누적합/BOJ_01806_부분합.java
@@ -0,0 +1,49 @@
+/**
+ * BOJ : 1806 G4 부분합
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_01806_부분합 {
+
+    static int N, S;
+    static int[] num;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        num = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        boolean check = false;
+        int left = 0, right = 0, sum = num[0], cnt = 1, res = N;
+        while (true) {
+            if (sum < S) {
+                right++;
+                if (right >= N) break;
+                cnt++;
+                sum += num[right];
+            } else {
+                check = true;
+                res = Math.min(res, cnt);
+                sum -= num[left];
+                left++;
+                cnt--;
+                if (left >= N) break;
+            }
+        }
+
+        if (check) System.out.println(res);
+        else System.out.println(0);
+    }
+}
