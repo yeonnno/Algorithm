b9c11fa7778d067928c7990a50ab3f4c495fce36
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-05-30 17:15:19 +0900

    BOJ : 2623 G3 음악프로그램

--- 위상정렬/BOJ_02623_음악프로그램.java
+++ 위상정렬/BOJ_02623_음악프로그램.java
@@ -12,8 +12,8 @@ import java.util.StringTokenizer;
 public class BOJ_02623_음악프로그램 {
 
     static int N, M;
-    static ArrayList<Integer>[] adj;
     static int[] indegree;
+    static ArrayList<Integer>[] adj;
     static ArrayList<Integer> res;
 
     public static void main(String[] args) throws IOException {
@@ -26,9 +26,8 @@ public class BOJ_02623_음악프로그램 {
         M = Integer.parseInt(st.nextToken());
 
         adj = new ArrayList[N + 1];
-        for (int i = 0; i <= N; i++) {
+        for (int i = 0; i <= N; i++)
             adj[i] = new ArrayList<>();
-        }
 
         indegree = new int[N + 1];
         for (int i = 0; i < M; i++) {
@@ -38,6 +37,7 @@ public class BOJ_02623_음악프로그램 {
             int x = Integer.parseInt(st.nextToken());
             for (int j = 0; j < len - 1; j++) {
                 int y = Integer.parseInt(st.nextToken());
+
                 adj[x].add(y);
                 indegree[y]++;
                 x = y;
@@ -48,11 +48,11 @@ public class BOJ_02623_음악프로그램 {
 
         topologySort();
 
-        if (res.size() != N) sb.append(0).append("\n");
+        if (res.size() != N)
+            sb.append("0\n");
         else {
-            for (int r : res) {
+            for (int r : res)
                 sb.append(r).append("\n");
-            }
         }
 
         System.out.print(sb);
@@ -63,17 +63,19 @@ public class BOJ_02623_음악프로그램 {
 
         for (int i = 1; i <= N; i++) {
             if (indegree[i] == 0)
-                Q.add(i);
+                Q.offer(i);
         }
 
         while (!Q.isEmpty()) {
             int now = Q.poll();
+
             res.add(now);
 
             for (int next : adj[now]) {
                 indegree[next]--;
+
                 if (indegree[next] == 0)
-                    Q.add(next);
+                    Q.offer(next);
             }
         }
     }
