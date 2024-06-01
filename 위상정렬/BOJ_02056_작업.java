78c775791be370df35a9a706da1be017141cce72
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-06-01 17:22:14 +0900

    BOJ : 2056 G4 작업

--- 위상정렬/BOJ_02056_작업.java
+++ 위상정렬/BOJ_02056_작업.java
@@ -12,8 +12,8 @@ import java.util.StringTokenizer;
 public class BOJ_02056_작업 {
 
     static int N, res;
+    static int[] indegree, delay, total;
     static ArrayList<Integer>[] adj;
-    static int[] indegree, time, total;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,15 +22,14 @@ public class BOJ_02056_작업 {
         N = Integer.parseInt(br.readLine());
 
         adj = new ArrayList[N + 1];
-        for (int i = 0; i <= N; i++) {
+        for (int i = 0; i <= N; i++)
             adj[i] = new ArrayList<>();
-        }
 
+        delay = new int[N + 1];
         indegree = new int[N + 1];
-        time = new int[N + 1];
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            time[i] = Integer.parseInt(st.nextToken());
+            delay[i] = Integer.parseInt(st.nextToken());
 
             int M = Integer.parseInt(st.nextToken());
             for (int j = 0; j < M; j++) {
@@ -46,9 +45,8 @@ public class BOJ_02056_작업 {
         topologySort();
 
         res = 0;
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++)
             res = Math.max(res, total[i]);
-        }
 
         System.out.println(res);
     }
@@ -57,7 +55,7 @@ public class BOJ_02056_작업 {
         Queue<Integer> Q = new LinkedList<>();
 
         for (int i = 1; i <= N; i++) {
-            total[i] = time[i];
+            total[i] = delay[i];
 
             if (indegree[i] == 0)
                 Q.offer(i);
@@ -67,9 +65,9 @@ public class BOJ_02056_작업 {
             int now = Q.poll();
 
             for (int next : adj[now]) {
-                total[next] = Math.max(total[next], total[now] + time[next]);
-
+                total[next] = Math.max(total[next], total[now] + delay[next]);
                 indegree[next]--;
+
                 if (indegree[next] == 0)
                     Q.offer(next);
             }
