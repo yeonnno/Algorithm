7791051f8f6ebfc5ddeacd4b009081c33add0d89
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-06-12 15:26:20 +0900

    BOJ : 21924 G4 도시 건설

--- 최소 스패닝 트리/BOJ_21924_도시건설.java
+++ 최소 스패닝 트리/BOJ_21924_도시건설.java
@@ -10,7 +10,7 @@ import java.util.StringTokenizer;
 public class BOJ_21924_도시건설 {
 
     static int N, M;
-    static long total, res;
+    static long total;
     static int[] parent;
     static PriorityQueue<Node> PQ;
 
@@ -23,47 +23,43 @@ public class BOJ_21924_도시건설 {
         M = Integer.parseInt(st.nextToken());
 
         parent = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++)
             parent[i] = i;
-        }
 
         total = 0;
         PQ = new PriorityQueue<>();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
 
+            PQ.offer(new Node(x, y, cost));
             total += cost;
-            PQ.offer(new Node(s, e, cost));
         }
-
-        res = 0;
 
         kruskal();
 
-        int tmp = find(1);
-        boolean check = true;
-        for (int i = 2; i <= N; i++) {
-            if (tmp != find(i)) check = false;
-        }
-
-        if (check) System.out.println(total - res);
+        if (check()) System.out.println(total);
         else System.out.println(-1);
+    }
+
+    private static boolean check() {
+        for (int i = 1; i <= N; i++) {
+            if (find(1) != find(i))
+                return false;
+        }
+        return true;
     }
 
     private static void kruskal() {
         while (!PQ.isEmpty()) {
             Node node = PQ.poll();
 
-            int x = find(node.s);
-            int y = find(node.e);
+            if (find(node.x) == find(node.y)) continue;
 
-            if (isSameParent(x, y)) continue;
-
-            res += node.cost;
-            union(node.s, node.e);
+            total -= node.cost;
+            union(node.x, node.y);
         }
     }
 
@@ -74,27 +70,19 @@ public class BOJ_21924_도시건설 {
         if (x != y) parent[y] = x;
     }
 
-    private static boolean isSameParent(int x, int y) {
-        x = find(x);
-        y = find(y);
-
-        if (x == y) return true;
-        else return false;
-    }
-
     private static int find(int x) {
         if (parent[x] == x) return x;
         else return parent[x] = find(parent[x]);
     }
 
     private static class Node implements Comparable<Node> {
-        int s;
-        int e;
+        int x;
+        int y;
         int cost;
 
-        Node(int s, int e, int cost) {
-            this.s = s;
-            this.e = e;
+        public Node(int x, int y, int cost) {
+            this.x = x;
+            this.y = y;
             this.cost = cost;
         }
 
