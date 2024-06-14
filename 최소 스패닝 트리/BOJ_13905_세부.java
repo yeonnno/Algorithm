80956a3473291774c7e1cb66cf747f7de606b5dd
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-06-14 15:12:49 +0900

    BOJ : 13905 G4 세부

--- 최소 스패닝 트리/BOJ_13905_세부.java
+++ 최소 스패닝 트리/BOJ_13905_세부.java
@@ -25,18 +25,17 @@ public class BOJ_13905_세부 {
         E = Integer.parseInt(st.nextToken());
 
         parent = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++)
             parent[i] = i;
-        }
 
         PQ = new PriorityQueue<>();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
 
-            PQ.offer(new Node(s, e, cost));
+            PQ.offer(new Node(x, y, cost));
         }
 
         res = 0;
@@ -51,15 +50,11 @@ public class BOJ_13905_세부 {
         while (!PQ.isEmpty()) {
             Node node = PQ.poll();
 
-            int x = find(node.s);
-            int y = find(node.e);
-
             if (find(S) == find(E)) return;
-            if (isSameParent(x, y)) continue;
+            if (find(node.x) == find(node.y)) continue;
 
             res = node.cost;
-
-            union(node.s, node.e);
+            union(node.x, node.y);
         }
     }
 
@@ -70,27 +65,19 @@ public class BOJ_13905_세부 {
         if (x != y) parent[y] = x;
     }
 
-    private static boolean isSameParent(int x, int y) {
-        x = find(x);
-        y = find(y);
-
-        if (x == y) return true;
-        else return false;
-    }
-
     private static int find(int x) {
         if (parent[x] == x) return x;
         else return parent[x] = find(parent[x]);
     }
 
     private static class Node implements Comparable<Node> {
-        int s;
-        int e;
+        int x;
+        int y;
         int cost;
 
-        Node(int s, int e, int cost) {
-            this.s = s;
-            this.e = e;
+        public Node(int x, int y, int cost) {
+            this.x = x;
+            this.y = y;
             this.cost = cost;
         }
 
