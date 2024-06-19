2747650e7fbafdd32187562317f857b31b10e495
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-06-19 17:40:58 +0900

    BOJ : 13418 G3 학교 탐방하기

--- 최소 스패닝 트리/BOJ_13418_학교탐방하기.java
+++ 최소 스패닝 트리/BOJ_13418_학교탐방하기.java
@@ -25,12 +25,12 @@ public class BOJ_13418_학교탐방하기 {
         PQ2 = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
         for (int i = 0; i < M + 1; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int cost = 1 ^ Integer.parseInt(st.nextToken());
 
-            PQ1.offer(new Node(s, e, 1 ^ cost));
-            PQ2.offer(new Node(s, e, 1 ^ cost));
+            PQ1.offer(new Node(x, y, cost));
+            PQ2.offer(new Node(x, y, cost));
         }
 
         int res1 = kruskal(PQ1);
@@ -41,21 +41,17 @@ public class BOJ_13418_학교탐방하기 {
 
     private static int kruskal(PriorityQueue<Node> PQ) {
         parent = new int[N + 1];
-        for (int i = 0; i <= N; i++) {
+        for (int i = 1; i <= N; i++)
             parent[i] = i;
-        }
 
         int cnt = 0;
         while (!PQ.isEmpty()) {
             Node node = PQ.poll();
 
-            int x = find(node.s);
-            int y = find(node.e);
-
-            if (isSameParent(x, y)) continue;
+            if (find(node.x) == find(node.y)) continue;
 
             cnt += node.cost;
-            union(node.s, node.e);
+            union(node.x, node.y);
         }
 
         return cnt;
@@ -68,27 +64,19 @@ public class BOJ_13418_학교탐방하기 {
         if (x != y) parent[y] = x;
     }
 
-    private static boolean isSameParent(int x, int y) {
-        x = find(x);
-        y = find(y);
-
-        if (x == y) return true;
-        else return false;
-    }
-
     private static int find(int x) {
         if (parent[x] == x) return x;
         else return parent[x] = find(parent[x]);
     }
 
     private static class Node {
-        int s;
-        int e;
+        int x;
+        int y;
         int cost;
 
-        Node(int s, int e, int cost) {
-            this.s = s;
-            this.e = e;
+        public Node(int x, int y, int cost) {
+            this.x = x;
+            this.y = y;
             this.cost = cost;
         }
     }
