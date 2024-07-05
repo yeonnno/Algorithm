a5eb5c8cd6d895c3f3beeeae3f96fef74500ef08
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-07-05 17:08:07 +0900

    BOJ : 14595 G3 동방 프로젝트 Large

--- 서로소 집합/BOJ_14595_동방프로젝트Large.java
+++ 서로소 집합/BOJ_14595_동방프로젝트Large.java
@@ -22,34 +22,30 @@ public class BOJ_14595_동방프로젝트Large {
         M = Integer.parseInt(br.readLine());
 
         parent = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++)
             parent[i] = i;
-        }
 
         if (M == 0) {
             merge = new int[1][2];
             merge[0][0] = merge[0][1] = 0;
         } else {
             merge = new int[M][2];
+
             for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-
-                merge[i][0] = x;
-                merge[i][1] = y;
+                merge[i][0] = Integer.parseInt(st.nextToken());
+                merge[i][1] = Integer.parseInt(st.nextToken());
             }
         }
 
         Arrays.sort(merge, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]);
 
-        int start = merge[0][0];
-        int end = merge[0][1];
+        int start = merge[0][0], end = merge[0][1];
         for (int i = 1; i < M; i++) {
             if (end < merge[i][0]) {
-                for (int j = start; j <= end; j++) {
+                for (int j = start; j <= end; j++)
                     union(start, j);
-                }
+
                 start = merge[i][0];
                 end = merge[i][1];
             } else if (end < merge[i][1]) {
@@ -57,14 +53,12 @@ public class BOJ_14595_동방프로젝트Large {
             }
         }
 
-        for (int i = start; i <= end; i++) {
+        for (int i = start; i <= end; i++)
             union(start, i);
-        }
 
         HashSet<Integer> set = new HashSet<>();
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++)
             set.add(find(i));
-        }
 
         System.out.println(set.size());
     }
