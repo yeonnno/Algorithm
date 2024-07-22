c3e566f4c8cad144ceac238c79f118fe88f8428d
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-07-22 17:48:05 +0900

    BOJ : 5639 G4 이진 검색 트리

---
+++ 트리/BOJ_05639_이진검색트리.java
@@ -0,0 +1,68 @@
+/**
+ * BOJ : 5639 G4 이진 검색 트리
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_05639_이진검색트리 {
+
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        Node root = new Node(Integer.parseInt(br.readLine()));
+
+        while (true) {
+            String s = br.readLine();
+
+            if (s == null || s.equals("")) break;
+
+            root.insert(Integer.parseInt(s));
+        }
+
+        postorder(root);
+
+        System.out.print(sb);
+    }
+
+    private static void postorder(Node node) {
+        if (node == null) return;
+
+        postorder(node.left);
+        postorder(node.right);
+        sb.append(node.val).append("\n");
+    }
+
+    private static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node left, Node right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public void insert(int n) {
+            if (n < this.val) {
+                if (this.left == null)
+                    this.left = new Node(n);
+                else
+                    this.left.insert(n);
+            } else {
+                if (this.right == null)
+                    this.right = new Node(n);
+                else
+                    this.right.insert(n);
+            }
+        }
+    }
+}
