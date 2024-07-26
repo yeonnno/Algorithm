b9dff2ec91d3bb56d2387452e32e373847e28daf
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-07-26 14:17:55 +0900

    BOJ : 1595 G4 북쪽나라의 도로

---
+++ 트리/BOJ_01595_북쪽나라의도로.java
@@ -0,0 +1,79 @@
+/**
+ * BOJ : 1595 G4 북쪽나라의 도로
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BOJ_01595_북쪽나라의도로 {
+
+    static int start, res;
+    static final int N = 10001;
+    static ArrayList<Node>[] tree;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        tree = new ArrayList[N];
+        for (int i = 0; i < N; i++)
+            tree[i] = new ArrayList<>();
+
+        while (true) {
+            int x, y, cost;
+
+            try {
+                st = new StringTokenizer(br.readLine());
+                x = Integer.parseInt(st.nextToken());
+                y = Integer.parseInt(st.nextToken());
+                cost = Integer.parseInt(st.nextToken());
+            } catch (Exception e) {
+                break;
+            }
+
+            tree[x].add(new Node(y, cost));
+            tree[y].add(new Node(x, cost));
+        }
+
+        start = 1;
+
+        res = Integer.MIN_VALUE;
+        visited = new boolean[N];
+        visited[start] = true;
+        DFS(start, 0);
+
+        res = Integer.MIN_VALUE;
+        visited = new boolean[N];
+        visited[start] = true;
+        DFS(start, 0);
+
+        System.out.println(res);
+    }
+
+    private static void DFS(int now, int sum) {
+        if (res < sum) {
+            res = sum;
+            start = now;
+        }
+
+        for (Node next : tree[now]) {
+            if (visited[next.x]) continue;
+
+            visited[next.x] = true;
+            DFS(next.x, sum + next.cost);
+        }
+    }
+
+    private static class Node {
+        int x;
+        int cost;
+
+        public Node(int x, int cost) {
+            this.x = x;
+            this.cost = cost;
+        }
+    }
+}
