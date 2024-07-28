5e813bb489a5b43f92d7af3c5a434b9dd08c1da1
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-07-28 17:01:40 +0900

    BOJ : 20955 G4 민서의 응급 수술

---
+++ 트리/BOJ_20955_민서의응급수술.java
@@ -0,0 +1,58 @@
+/**
+ * BOJ : 20955 G4 민서의 응급 수술
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class BOJ_20955_민서의응급수술 {
+
+    static int N, M, res;
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++)
+            parent[i] = i;
+
+        res = 0;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (find(x) == find(y)) {
+                res++;
+            } else {
+                union(x, y);
+            }
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 1; i <= N; i++)
+            set.add(find(i));
+
+        System.out.println(res + set.size() - 1);
+    }
+
+    private static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x != y) parent[y] = x;
+    }
+
+    private static int find(int x) {
+        if (parent[x] == x) return x;
+        else return parent[x] = find(parent[x]);
+    }
+}
