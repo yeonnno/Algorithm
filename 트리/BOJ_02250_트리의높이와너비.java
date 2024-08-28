f072b71cd652a823c386248a43ad325e8004c760
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-08-28 17:42:04 +0900

    BOJ : 2250 G2 트리의 높이와 너비

--- 트리/BOJ_02250_트리의높이와너비.java
+++ 트리/BOJ_02250_트리의높이와너비.java
@@ -18,9 +18,9 @@ public class BOJ_02250_트리의높이와너비 {
 
         N = Integer.parseInt(br.readLine());
 
+        tree = new Node[N + 1];
         minLevel = new int[N + 1];
         maxLevel = new int[N + 1];
-        tree = new Node[N + 1];
         for (int i = 1; i <= N; i++) {
             tree[i] = new Node(-1, -1);
             minLevel[i] = N;
@@ -57,28 +57,29 @@ public class BOJ_02250_트리의높이와너비 {
         width = maxLevel[1] - minLevel[1] + 1;
         for (int i = 2; i <= totalLevel; i++) {
             int w = maxLevel[i] - minLevel[i] + 1;
+
             if (width < w) {
-                level = i;
                 width = w;
+                level = i;
             }
         }
 
         System.out.println(level + " " + width);
     }
 
-    private static void inorder(Node node, int level) {
-        if (totalLevel < level)
-            totalLevel = level;
+    private static void inorder(Node node, int lv) {
+        if (totalLevel < lv)
+            totalLevel = lv;
 
         if (node.left != -1)
-            inorder(tree[node.left], level + 1);
+            inorder(tree[node.left], lv + 1);
 
-        minLevel[level] = Math.min(minLevel[level], idx);
-        maxLevel[level] = idx;
+        minLevel[lv] = Math.min(minLevel[lv], idx);
+        maxLevel[lv] = idx;
         idx++;
 
         if (node.right != -1)
-            inorder(tree[node.right], level + 1);
+            inorder(tree[node.right], lv + 1);
     }
 
     private static class Node {
