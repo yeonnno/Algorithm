df615b9f1077d3b9f317d6f46a4f4940de03c5b1
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-09-02 20:16:35 +0900

    BOJ : 1949 G2 우수 마을

--- 트리/BOJ_01949_우수마을.java
+++ 트리/BOJ_01949_우수마을.java
@@ -10,7 +10,7 @@ import java.util.StringTokenizer;
 public class BOJ_01949_우수마을 {
 
     static int N;
-    static int[] num;
+    static int[] people;
     static int[][] dp;
     static boolean[] visited;
     static ArrayList<Integer>[] tree;
@@ -21,14 +21,14 @@ public class BOJ_01949_우수마을 {
 
         N = Integer.parseInt(br.readLine());
 
-        num = new int[N + 1];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++)
-            num[i] = Integer.parseInt(st.nextToken());
-
         tree = new ArrayList[N + 1];
         for (int i = 0; i <= N; i++)
             tree[i] = new ArrayList<>();
+
+        people = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++)
+            people[i] = Integer.parseInt(st.nextToken());
 
         for (int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
@@ -49,7 +49,8 @@ public class BOJ_01949_우수마을 {
 
     private static void DFS(int now) {
         visited[now] = true;
-        dp[1][now] = num[now];
+
+        dp[1][now] = people[now];
 
         for (int next : tree[now]) {
             if (visited[next]) continue;
