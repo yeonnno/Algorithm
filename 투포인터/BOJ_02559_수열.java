58513fe2ad8c9a34853a0dea3187d0ae14757026
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-10-22 14:23:26 +0900

    BOJ : 2559 S3 수열

--- 투포인터/BOJ_02559_수열.java
+++ 투포인터/BOJ_02559_수열.java
@@ -8,8 +8,8 @@ import java.util.StringTokenizer;
 
 public class BOJ_02559_수열 {
 
-    static int N, K, res;
-    static int[] temperature;
+    static int N, K, sum, res;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,25 +19,22 @@ public class BOJ_02559_수열 {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        temperature = new int[N];
+        arr = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++)
-            temperature[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
 
-        int sum = 0;
+        sum = 0;
         for (int i = 0; i < K; i++)
-            sum += temperature[i];
+            sum += arr[i];
 
         res = sum;
         int start = 0, end = K;
-        while (end != N) {
-            sum -= temperature[start];
-            sum += temperature[end];
+        while (end < N) {
+            sum -= arr[start++];
+            sum += arr[end++];
 
             res = Math.max(res, sum);
-
-            start++;
-            end++;
         }
 
         System.out.println(res);
