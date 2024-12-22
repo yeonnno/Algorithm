fcfd3197fb263ce29499d455d026ca303753a3f7
yeonnno <109949649+yeonnno@users.noreply.github.com>
2024-12-22 20:57:49 +0900

    BOJ : 14891 G5 톱니바퀴

--- 시뮬레이션/BOJ_14891_톱니바퀴.java
+++ 시뮬레이션/BOJ_14891_톱니바퀴.java
@@ -10,17 +10,18 @@ public class BOJ_14891_톱니바퀴 {
 
     static int K, res;
     static int[] dir;
-    static int[][] gear;
+    static boolean[][] gear;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
 
-        gear = new int[4][8];
+        gear = new boolean[4][8];
         for (int i = 0; i < 4; i++) {
             String s = br.readLine();
             for (int j = 0; j < 8; j++) {
-                gear[i][j] = s.charAt(j) - '0';
+                if (s.charAt(j) == '1')
+                    gear[i][j] = true;
             }
         }
 
@@ -34,20 +35,20 @@ public class BOJ_14891_톱니바퀴 {
             dir[gearNum] = turn;
 
             checkDir(gearNum);
-            turnGear();
+            turnDir();
         }
 
         res = 0;
-        if (gear[0][0] == 1) res += 1;
-        if (gear[1][0] == 1) res += 2;
-        if (gear[2][0] == 1) res += 4;
-        if (gear[3][0] == 1) res += 8;
+        if (gear[0][0]) res += 1;
+        if (gear[1][0]) res += 2;
+        if (gear[2][0]) res += 4;
+        if (gear[3][0]) res += 8;
 
         System.out.println(res);
     }
 
-    private static void turnGear() {
-        int tmp = 0;
+    private static void turnDir() {
+        boolean tmp;
         for (int i = 0; i < 4; i++) {
             if (dir[i] == 1) {
                 tmp = gear[i][7];
@@ -65,14 +66,14 @@ public class BOJ_14891_톱니바퀴 {
 
     private static void checkDir(int n) {
         for (int i = n - 1; i >= 0; i--) {
-            if (gear[i][2] != gear[i + 1][6])
+            if (gear[i][2] ^ gear[i + 1][6])
                 dir[i] = -dir[i + 1];
             else
                 break;
         }
 
         for (int i = n + 1; i < 4; i++) {
-            if (gear[i][6] != gear[i - 1][2])
+            if (gear[i][6] ^ gear[i - 1][2])
                 dir[i] = -dir[i - 1];
             else
                 break;
