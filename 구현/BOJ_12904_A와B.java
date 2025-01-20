e3e25bc951c599c349fa296ef1c981e66faf3ce2
yeonnno <109949649+yeonnno@users.noreply.github.com>
2025-01-20 19:21:39 +0900

    BOJ : 12904 G5 A와 B

---
+++ 구현/BOJ_12904_A와B.java
@@ -0,0 +1,28 @@
+/**
+ * BOJ : 12904 G5 A와 B
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_12904_A와B {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder S = new StringBuilder(br.readLine());
+        StringBuilder T = new StringBuilder(br.readLine());
+
+        while (S.length() < T.length()) {
+            int tLen = T.length();
+
+            if (T.charAt(tLen - 1) == 'A') {
+                T.deleteCharAt(tLen - 1);
+            } else if (T.charAt(tLen - 1) == 'B') {
+                T.deleteCharAt(tLen - 1);
+                T.reverse();
+            }
+        }
+
+        System.out.println(S.toString().equals(T.toString()) ? 1 : 0);
+    }
+}
