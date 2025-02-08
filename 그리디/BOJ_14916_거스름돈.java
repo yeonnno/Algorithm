275a4aaf17d5ccb102b0139e6fbc15986669d604
yeonnno <109949649+yeonnno@users.noreply.github.com>
2025-02-08 19:56:14 +0900

    BOJ : S5 14916 거스름돈

---
+++ 그리디/BOJ_14916_거스름돈.java
@@ -0,0 +1,36 @@
+/**
+ * BOJ : S5 14916 거스름돈
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_14916_거스름돈 {
+
+    static int N, res;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        res = 0;
+
+        while (true) {
+            if (N < 0) {
+                res = -1;
+                break;
+            }
+
+            if (N % 5 == 0) {
+                res += N / 5;
+                break;
+            } else {
+                N -= 2;
+                res++;
+            }
+        }
+
+        System.out.println(res);
+    }
+}
