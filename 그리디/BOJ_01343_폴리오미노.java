e393c822304f099fd5f975cde44b2a2c3d46b2f8
yeonnno <109949649+yeonnno@users.noreply.github.com>
2025-02-09 15:00:20 +0900

    BOJ : 1343 S5 폴리오미노

---
+++ 그리디/BOJ_01343_폴리오미노.java
@@ -0,0 +1,50 @@
+/**
+ * BOJ : 1343 S5 폴리오미노
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_01343_폴리오미노 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String board = br.readLine() + '.';
+        int len = board.length();
+
+        int cnt = 0;
+        boolean check = true;
+        for (int i = 0; i < len; i++) {
+            char ch = board.charAt(i);
+
+            if (ch == 'X') {
+                cnt++;
+            } else {
+                if (cnt % 2 != 0) {
+                    check = false;
+                    break;
+                }
+
+                while (cnt != 0) {
+                    if (cnt >= 4) {
+                        cnt -= 4;
+                        sb.append("AAAA");
+                    } else {
+                        cnt -= 2;
+                        sb.append("BB");
+                    }
+                }
+
+                sb.append(".");
+            }
+        }
+
+        if (check) {
+            sb.deleteCharAt(sb.length() - 1);
+            System.out.println(sb);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
