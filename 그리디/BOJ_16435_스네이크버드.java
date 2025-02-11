7a11d036ca9e799fd4f3633c0108fbee5a8ae882
yeonnno <109949649+yeonnno@users.noreply.github.com>
2025-02-11 18:34:33 +0900

    BOJ : 16435 S5 스네이크버드

---
+++ 그리디/BOJ_16435_스네이크버드.java
@@ -0,0 +1,39 @@
+/**
+ * BOJ : 16435 S5 스네이크버드
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_16435_스네이크버드 {
+
+    static int N, L;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            if (arr[i] <= L)
+                L++;
+            else
+                break;
+        }
+
+        System.out.println(L);
+    }
+}
