1771f491c5e4d5e50b0f066301a9b0ef9239bf0a
yeonnno <109949649+yeonnno@users.noreply.github.com>
2025-02-15 15:00:57 +0900

    BOJ : 11047 S4 동전 0

---
+++ 그리디/BOJ_11047_동전0.java
@@ -0,0 +1,32 @@
+/**
+ * BOJ : 11047 S4 동전 0
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11047_동전0 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] coin = new int[N];
+        for (int i = 0; i < N; i++)
+            coin[i] = Integer.parseInt(br.readLine());
+
+        int res = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            if (coin[i] > K) continue;
+
+            res += K / coin[i];
+            K %= coin[i];
+        }
+
+        System.out.println(res);
+    }
+}
