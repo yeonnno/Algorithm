c41661f86c0c07c0cf7467f66b997bb2d411951d
yeonnno <109949649+yeonnno@users.noreply.github.com>
2025-02-22 18:13:21 +0900

    BOJ : 20115 S3 에너지 드링크

---
+++ 그리디/BOJ_20115_에너지드링크.java
@@ -0,0 +1,34 @@
+/**
+ * BOJ : 20115 S3 에너지 드링크
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_20115_에너지드링크 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int N = Integer.parseInt(br.readLine());
+
+        double max = 0;
+        double[] arr = new double[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+
+            max = Math.max(max, arr[i]);
+        }
+
+        double res = max;
+        for (int i = 0; i < N; i++) {
+            if (max == arr[i]) continue;
+
+            res += arr[i] / 2;
+        }
+
+        System.out.println(res);
+    }
+}
