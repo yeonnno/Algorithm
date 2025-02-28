5fd656c13bba34a42745a1cc91940949b9a7d8f5
yeonnno <109949649+yeonnno@users.noreply.github.com>
2025-02-28 16:51:29 +0900

    BOJ : 1138 S2 한 줄로 서기

---
+++ 그리디/BOJ_01138_한줄로서기.java
@@ -0,0 +1,41 @@
+/**
+ * BOJ : 1138 S2 한 줄로 서기
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_01138_한줄로서기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        int[] res = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            int j = 1;
+
+            while (true) {
+                if (arr[i] == 0 && res[j] == 0) {
+                    res[j] = i;
+                    break;
+                } else if (res[j] == 0) {
+                    arr[i]--;
+                }
+
+                j++;
+            }
+        }
+
+        for (int i = 1; i <= N; i++)
+            System.out.print(res[i] + " ");
+    }
+}
