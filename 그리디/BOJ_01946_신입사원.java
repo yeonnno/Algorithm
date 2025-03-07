2333bdaad508480381d76196b1f024a1bf41e03f
yeonnno <109949649+yeonnno@users.noreply.github.com>
2025-03-07 16:47:58 +0900

    BOJ : 1946 S1 신입 사원

---
+++ 그리디/BOJ_01946_신입사원.java
@@ -0,0 +1,41 @@
+/**
+ * BOJ : 1946 S1 신입 사원
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_01946_신입사원 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken()) - 1;
+                int y = Integer.parseInt(st.nextToken());
+
+                arr[x] = y;
+            }
+
+            int res = 1, min = arr[0];
+            for (int i = 1; i < N; i++) {
+                if (min > arr[i]) {
+                    min = arr[i];
+                    res++;
+                }
+            }
+
+            sb.append(res).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
