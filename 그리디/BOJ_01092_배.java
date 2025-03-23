f75d249edc8f01c1b43a9b5f1a80a3173016ec3f
yeonnno <109949649+yeonnno@users.noreply.github.com>
2025-03-23 18:30:40 +0900

    BOJ : 1095 G5 배

---
+++ 그리디/BOJ_01092_배.java
@@ -0,0 +1,61 @@
+/**
+ * BOJ : 1095 G5 배
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_01092_배 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int N = Integer.parseInt(br.readLine());
+
+        List<Integer> crane = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            crane.add(Integer.parseInt(st.nextToken()));
+
+        crane.sort(Collections.reverseOrder());
+
+        int M = Integer.parseInt(br.readLine());
+
+        List<Integer> box = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++)
+            box.add(Integer.parseInt(st.nextToken()));
+
+        box.sort(Collections.reverseOrder());
+
+        if (crane.get(0) < box.get(0)) {
+            System.out.println(-1);
+            return;
+        }
+
+        int res = 0;
+        while (!box.isEmpty()) {
+            int boxIdx = 0, craneIdx = 0;
+
+            while (craneIdx < N) {
+                if (boxIdx == box.size())
+                    break;
+
+                if (crane.get(craneIdx) >= box.get(boxIdx)) {
+                    box.remove(boxIdx);
+                    craneIdx++;
+                } else {
+                    boxIdx++;
+                }
+            }
+
+            res++;
+        }
+
+        System.out.println(res);
+    }
+}
