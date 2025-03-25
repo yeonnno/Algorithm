bb1c7b4b0e563ebba1e71014c6db0f8ffb18ca4c
yeonnno <109949649+yeonnno@users.noreply.github.com>
2025-03-25 18:34:59 +0900

    BOJ : 13164 G5 행복 유치원

---
+++ 그리디/BOJ_13164_행복유치원.java
@@ -0,0 +1,39 @@
+/**
+ * BOJ : 13164 G5 행복 유치원
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_13164_행복유치원 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i < N; i++)
+            list.add(arr[i] - arr[i - 1]);
+
+        Collections.sort(list);
+
+        int res = 0;
+        for (int i = 0; i < N - K; i++)
+            res += list.get(i);
+
+        System.out.println(res);
+    }
+}
