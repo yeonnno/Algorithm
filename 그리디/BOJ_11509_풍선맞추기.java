37b26904a9b01fcc9834de5e9ffd116e37a0f650
yeonnno <109949649+yeonnno@users.noreply.github.com>
2025-04-08 18:24:09 +0900

    BOJ : 11509 G5 풍선 맞추기

---
+++ 그리디/BOJ_11509_풍선맞추기.java
@@ -0,0 +1,36 @@
+/**
+ * BOJ : 11509 G5 풍선 맞추기
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11509_풍선맞추기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        int[] arrow = new int[1000001];
+
+        int res = 0;
+        for (int i = 0; i < N; i++) {
+            if (arrow[arr[i]] > 0) {
+                arrow[arr[i]]--;
+                arrow[arr[i] - 1]++;
+            } else {
+                res++;
+                arrow[arr[i] - 1]++;
+            }
+        }
+
+        System.out.println(res);
+    }
+}
