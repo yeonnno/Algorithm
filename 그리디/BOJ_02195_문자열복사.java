38844237a2f72d9e2e336d9ab98481e22a115dc2
yeonnno <109949649+yeonnno@users.noreply.github.com>
2025-04-13 15:41:24 +0900

    BOJ : 2195 G5 문자열 복사

---
+++ 그리디/BOJ_02195_문자열복사.java
@@ -0,0 +1,27 @@
+/**
+ * BOJ : 2195 G5 문자열 복사
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_02195_문자열복사 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = br.readLine();
+        String P = br.readLine();
+
+        int res = 0, idx = 0, len = P.length();
+        for (int i = 0; i < len; i++) {
+            String sub = P.substring(idx, i + 1);
+
+            if (S.indexOf(sub) == -1) {
+                res++;
+                idx = i;
+            }
+        }
+
+        System.out.println(res + 1);
+    }
+}
