4aedb2c32354f8ae61f502bbe13af94013f8747c
yeonnno <109949649+yeonnno@users.noreply.github.com>
2025-05-22 18:02:58 +0900

    BOJ : 2212 G5 센서

--- 그리디/BOJ_02212_센서.java
+++ 그리디/BOJ_02212_센서.java
@@ -15,28 +15,27 @@ public class BOJ_02212_센서 {
         int N = Integer.parseInt(br.readLine());
         int K = Integer.parseInt(br.readLine());
 
-        if (K >= N) {
+        if (N <= K) {
             System.out.println(0);
-            return;
+        } else {
+            int[] arr = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(st.nextToken());
+
+            Arrays.sort(arr);
+
+            int[] diff = new int[N - 1];
+            for (int i = 0; i < N - 1; i++)
+                diff[i] = arr[i + 1] - arr[i];
+
+            Arrays.sort(diff);
+
+            int res = 0;
+            for (int i = 0; i < N - K; i++)
+                res += diff[i];
+
+            System.out.println(res);
         }
-
-        int[] arr = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
-
-        Arrays.sort(arr);
-
-        int[] diff = new int[N - 1];
-        for (int i = 0; i < N - 1; i++)
-            diff[i] = arr[i + 1] - arr[i];
-
-        Arrays.sort(diff);
-
-        int res = 0;
-        for (int i = 0; i < N - K; i++)
-            res += diff[i];
-
-        System.out.println(res);
     }
 }
