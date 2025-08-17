b5b34481aaee846650c29b6bff0d73f729c44e38
yeonnno <109949649+yeonnno@users.noreply.github.com>
2025-08-17 15:24:23 +0900

    BOJ : 11123 S2 양 한마리 양 두마리

---
+++ 그래프탐색/BOJ_11123_양한마리양두마리.java
@@ -0,0 +1,92 @@
+/**
+ * BOJ : 11123 S2 양 한마리 양 두마리
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_11123_양한마리양두마리 {
+
+    static int N, M, res;
+    static boolean[][] map, visited;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+
+            map = new boolean[N][M];
+            for (int i = 0; i < N; i++) {
+                String s = br.readLine();
+                for (int j = 0; j < M; j++) {
+                    char ch = s.charAt(j);
+
+                    if (ch == '#')
+                        map[i][j] = true;
+                }
+            }
+
+            res = 0;
+            visited = new boolean[N][M];
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (map[i][j] && !visited[i][j]) {
+                        res++;
+                        BFS(i, j);
+                    }
+                }
+            }
+
+            sb.append(res).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    private static void BFS(int x, int y) {
+        Queue<Point> Q = new LinkedList<>();
+        Q.offer(new Point(x, y));
+
+        visited[x][y] = true;
+
+        while (!Q.isEmpty()) {
+            Point now = Q.poll();
+
+            for (int d = 0; d < 4; d++) {
+                int nx = now.x + dx[d];
+                int ny = now.y + dy[d];
+
+                if (!isPossible(nx, ny) || visited[nx][ny]) continue;
+                if (!map[nx][ny]) continue;
+
+                visited[nx][ny] = true;
+                Q.offer(new Point(nx, ny));
+            }
+        }
+    }
+
+    private static boolean isPossible(int nx, int ny) {
+        return nx >= 0 && nx < N && ny >= 0 && ny < M;
+    }
+
+    private static class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
