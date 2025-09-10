517cdf2cf54fc3151faad6637bc221c4b34feeab
yeonnno <109949649+yeonnno@users.noreply.github.com>
2025-09-10 18:28:12 +0900

    BOJ : 16928 G5 뱀과 사다리 게임

--- 그래프탐색/BOJ_16928_뱀과사다리게임.java
+++ 그래프탐색/BOJ_16928_뱀과사다리게임.java
@@ -11,7 +11,7 @@ import java.util.StringTokenizer;
 public class BOJ_16928_뱀과사다리게임 {
 
     static int N, M;
-    static int[] map, check;
+    static int[] map, res;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,9 +22,8 @@ public class BOJ_16928_뱀과사다리게임 {
         M = Integer.parseInt(st.nextToken());
 
         map = new int[101];
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i < 101; i++)
             map[i] = i;
-        }
 
         for (int i = 0; i < N + M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -34,32 +33,32 @@ public class BOJ_16928_뱀과사다리게임 {
             map[x] = y;
         }
 
-        check = new int[101];
-        BFS(1);
+        res = new int[101];
 
-        System.out.println(check[100]);
+        BFS();
+
+        System.out.println(res[100]);
     }
 
-    private static void BFS(int start) {
+    private static void BFS() {
         Queue<Integer> Q = new LinkedList<>();
-        Q.add(start);
+        Q.offer(1);
 
         while (!Q.isEmpty()) {
             int now = Q.poll();
 
-            for (int i = 1; i <= 6; i++) {
-                int next = now + i;
+            for (int d = 1; d <= 6; d++) {
+                int next = now + d;
 
-                if (next > 100) continue;
+                if (next > 100) break;
 
-                if (check[map[next]] == 0) {
-                    Q.add(map[next]);
-                    check[map[next]] = check[now] + 1;
+                if (res[map[next]] == 0) {
+                    res[map[next]] = res[now] + 1;
+                    Q.offer(map[next]);
                 }
 
-                if (map[next] == 100) {
+                if (res[next] == 100)
                     return;
-                }
             }
         }
     }
