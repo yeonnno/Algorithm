c129cc82a122819e3cbd6d585ce0b87c46ad1570
yeonnno <109949649+yeonnno@users.noreply.github.com>
2025-09-15 16:50:46 +0900

    BOJ : 17836 G5 공주님을 구해라!

--- 그래프탐색/BOJ_17836_공주님을구해라.java
+++ 그래프탐색/BOJ_17836_공주님을구해라.java
@@ -11,6 +11,7 @@ import java.util.StringTokenizer;
 public class BOJ_17836_공주님을구해라 {
 
     static int N, M, T;
+    static final int MAX = 10001;
     static int[][] map, dist;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
@@ -23,72 +24,67 @@ public class BOJ_17836_공주님을구해라 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         T = Integer.parseInt(st.nextToken());
-        int max = 10001;
 
         map = new int[N][M];
         dist = new int[N][M];
-        int swordX = -1;
-        int swordY = -1;
+        Point sword = new Point(-1, -1);
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+
                 if (map[i][j] == 2) {
-                    swordX = i;
-                    swordY = j;
+                    sword.x = i;
+                    sword.y = j;
                 }
-                dist[i][j] = max;
+
+                dist[i][j] = MAX;
             }
         }
 
-        BFS(new Point(0, 0));
+        BFS();
 
         int res1 = dist[N - 1][M - 1];
-        int res2 = dist[swordX][swordY] + ((N - 1 - swordX) + (M - 1 - swordY));
+        int res2 = dist[sword.x][sword.y] + ((N - sword.x - 1) + (M - sword.y - 1));
         int min = Math.min(res1, res2);
 
-        if (min > T) {
+        if (min > T)
             System.out.println("Fail");
-        } else {
+        else
             System.out.println(min);
-        }
     }
 
-    private static void BFS(Point p) {
+    private static void BFS() {
         Queue<Point> Q = new LinkedList<>();
-        Q.add(p);
-        dist[p.x][p.y] = 0;
+        Q.offer(new Point(0, 0));
+
+        dist[0][0] = 0;
 
         while (!Q.isEmpty()) {
             Point now = Q.poll();
-            int x = now.x;
-            int y = now.y;
 
             for (int d = 0; d < 4; d++) {
-                int nx = x + dx[d];
-                int ny = y + dy[d];
+                int nx = now.x + dx[d];
+                int ny = now.y + dy[d];
 
-                if (!isPossible(nx, ny)) continue;
-                if (map[nx][ny] == 1) continue;
+                if (!isPossible(nx, ny) || map[nx][ny] == 1) continue;
 
-                if (dist[nx][ny] > dist[x][y] + 1) {
-                    dist[nx][ny] = dist[x][y] + 1;
-                    Q.add(new Point(nx, ny));
+                if (dist[nx][ny] > dist[now.x][now.y] + 1) {
+                    dist[nx][ny] = dist[now.x][now.y] + 1;
+                    Q.offer(new Point(nx, ny));
                 }
             }
         }
     }
 
     private static boolean isPossible(int nx, int ny) {
-        if (nx >= 0 && nx < N && ny >= 0 && ny < M) return true;
-        else return false;
+        return nx >= 0 && nx < N && ny >= 0 && ny < M;
     }
 
-    static class Point {
-        int x;
-        int y;
+    private static class Point {
+        int x, y;
 
-        Point(int x, int y) {
+        public Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
